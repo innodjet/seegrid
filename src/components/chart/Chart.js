a6ff29118d370a7b0ed2ecc35f3b3e6d7a3f381f
innodjet@gmail.com <innodjet@gmail.com>
2020-08-05 17:51:13 -0500

    refacto:eliminating countries with null percentage from the chart display

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -4,16 +4,20 @@ import "./Chart.scss";
 const Chart = ({ chartData, worldPopulation, selectedYear }) => {
   const chartDisplay = () => {
     return chartData.map((el) => {
-      const { Country_Code } = el;
+      const { Country_Code, Country } = el;
       const population = el[`Year_${selectedYear}`];
       const percentage = Math.round((population / worldPopulation) * 100);
-      return (
-        <div className="chart__list" key={Country_Code}>
-          <dd className={`percentage percentage-${percentage}`}>
-            <span className="text">{`${Country_Code}`}</span>
-          </dd>
-        </div>
-      );
+      let rep;
+      if (percentage > 0) {
+        rep = (
+          <div className="chart__list" key={Country_Code}>
+            <dd className={`percentage percentage-${percentage}`}>
+              <span className="text">{`${Country} (${Country_Code})`}</span>
+            </dd>
+          </div>
+        );
+      }
+      return rep;
     });
   };
   return (
